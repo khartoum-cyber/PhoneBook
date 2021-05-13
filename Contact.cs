187a086e411539ca208db7a4338fb04f44214851
bartosz0 <methkill@gmail.com>
2021-05-13 12:30:07 +0200

    Added Contact class with props and ctor

---
+++ Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook
+{
+    public class Contact
+    {
+        private Contact(string name, int number)
+        {
+            Name = name;
+            Number = number;
+        }
+
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
